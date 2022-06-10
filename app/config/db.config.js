56254c5c02e46cb41c6f01547d5a03ac1e4d6199
kunal-relan <kunal.relan@hotmail.com>
2022-06-10 13:33:18 +0530

    makes minor changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb"
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USERNAME,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME
 };
